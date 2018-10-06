49f95dad8813aec050fc2115618544c3e411c7ef
brent johnson <brentj433@gmail.com>
2018-10-05 19:39:44 -0700

    test dynamic allocates Room

--- test.cpp
+++ test.cpp
@@ -5,13 +5,16 @@
 
 int main()
 {
-	Room room("rooms/foyer");
-	std::cout << room.getRoomName() << "\n\n";
-	std::cout << room.getLongDesc() << "\n\n";
-	std::cout << room.getShortDesc() << "\n\n";
-	std::cout << room.getAdditionalDesc() << "\n\n";
-	std::cout << room.getLongExitDesc() << "\n\n";
-	std::cout << room.getShortExitDesc() << "\n";
+	Room *room;
+	room = new Room("rooms/foyer");
+
+	std::cout << room->getRoomName() << "\n\n";
+	std::cout << room->getLongDesc() << "\n\n";
+	std::cout << room->getShortDesc() << "\n\n";
+	std::cout << room->getAdditionalDesc() << "\n\n";
+	std::cout << room->getLongExitDesc() << "\n\n";
+	std::cout << room->getShortExitDesc() << "\n";
+	delete room;
 
 	return 0;
 }
